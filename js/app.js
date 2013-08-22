ab732560143386d3ac2f3d9a1f28ac9674613ef3
marklu <marklu@marklu.net>
2013-08-21 19:59:18 -0700

    context automatically generated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 define(function(require, exports, module) {
     var FamousEngine = require('famous/Engine');
     
+    var mainCtx = FamousEngine.createContext();
+    mainCtx.setPerspective(1000);
+
     // your app here
 });
