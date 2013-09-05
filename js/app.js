9330c650638a2ff09c99c03672cb97b784a62f3d
marklu <marklu@marklu.net>
2013-09-05 16:06:25 -0700

    simplified app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 define(function(require, exports, module) {
+    // import dependencies
     var FamousEngine = require('famous/Engine');
-    
+   
+    // create the main context
     var mainCtx = FamousEngine.createContext();
-    mainCtx.setPerspective(1000);
-
+    
+    
     // your app here
+      
+
 });
