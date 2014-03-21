8b973b9a8a1e6c0a5f865dc14e54509802941475
Mike O'Brien <michael.obrien.a@gmail.com>
2014-03-21 15:42:58 -0700

    empty main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
 define(function(require, exports, module) {
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-   
-    // create the main context
-    var mainContext = Engine.createContext();
-    
-    
-    // your app here
-      
-
+	// import your example
+	require('examples/core/Engine');
 });
