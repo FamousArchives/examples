06c98d44631d78746517ee05d5b8553385625ed9
Mike O'Brien <michael.obrien.a@gmail.com>
2014-03-21 15:30:15 -0700

    blank files added

---
+++ src/examples/widgets/ToggleButton/example.js
@@ -0,0 +1,12 @@
+define(function(require, exports, module) {
+    // import dependencies
+    var Engine = require('famous/core/Engine');
+   
+    // create the main context
+    var mainContext = Engine.createContext();
+    
+    
+    // your app here
+      
+
+});
