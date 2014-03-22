0f368493065ddef9041f9e12f4575c9f669fe6c3
drclick <drclick@mac.com>
2014-03-22 13:55:48 -0700

    done

--- src/examples/transitions/WallTransition/example.js
+++ src/examples/transitions/WallTransition/example.js
@@ -1,12 +1,35 @@
 define(function(require, exports, module) {
     // import dependencies
-    var Engine = require('famous/core/Engine');
-   
+    var Engine 				= require("famous/core/Engine");
+    var Surface 			= require("famous/core/Surface");
+    var Modifier 			= require("famous/core/Modifier");
+    var Transform 			= require("famous/core/Transform");
+
+    var Transitionable  	= require("famous/transitions/Transitionable");
+    var WallTransition		= require("famous/transitions/WallTransition");
+
+   	
     // create the main context
     var mainContext = Engine.createContext();
-    
-    
-    // your app here
-      
 
+    var surface = new Surface({
+    	size:[100,100],
+    	classes: ['famousRedBackground'],
+    	properties: {
+    		borderRadius: "50px"
+    	}
+    });
+
+    var modifier = new Modifier({
+    	origin: [.5,.5]
+    });
+
+    // debugger
+	Transitionable.registerMethod('wall', WallTransition);
+
+	var transition = {method: 'wall', dampingRatio : 0, period : 500};
+	
+    modifier.setTransform(Transform.translate(0,100,0),transition);
+
+    mainContext.add(modifier).add(surface);
 });
