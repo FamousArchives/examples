3093e802ef69dbd394d225a7306e9631253f689a
drclick <drclick@mac.com>
2014-03-22 13:56:29 -0700

    done

--- src/examples/transitions/SpringTransition/example.js
+++ src/examples/transitions/SpringTransition/example.js
@@ -1,12 +1,36 @@
 define(function(require, exports, module) {
     // import dependencies
-    var Engine = require('famous/core/Engine');
-   
+    var Engine 				= require("famous/core/Engine");
+    var Surface 			= require("famous/core/Surface");
+    var Modifier 			= require("famous/core/Modifier");
+    var Transform 			= require("famous/core/Transform");
+
+    var Transitionable  	= require("famous/transitions/Transitionable");
+    var SpringTransition	= require("famous/transitions/SpringTransition");
+
+   	
     // create the main context
     var mainContext = Engine.createContext();
-    
-    
-    // your app here
-      
 
+    var surface = new Surface({
+    	size:[100,100],
+    	classes: ['famousRedBackground'],
+    	properties: {
+    		borderRadius: "50px"
+    	}
+    });
+
+    var modifier = new Modifier({
+    	origin: [.5,.5]
+    });
+
+	Transitionable.registerMethod('spring', SpringTransition);
+	var transition = {method: "spring"}
+
+
+	
+    modifier.setTransform(Transform.translate(0,100,0),transition);
+
+    mainContext.add(modifier).add(surface);
 });
+
