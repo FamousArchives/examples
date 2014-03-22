7f37c7d8563c88d2501e64a0ccccfb8d64305c9e
Mike O'Brien <michael.obrien.a@gmail.com>
2014-03-22 08:22:15 -0700

    added color example

--- src/examples/utilities/Color/example.js
+++ src/examples/utilities/Color/example.js
@@ -1,12 +1,29 @@
 define(function(require, exports, module) {
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-   
-    // create the main context
-    var mainContext = Engine.createContext();
-    
-    
-    // your app here
-      
+     var Engine = require('famous/core/Engine');
+     var Surface = require('famous/core/Surface');
+     var Color = require('famous/utilities/Color');
+     var Context = Engine.createContext();
+     
+     var color = new Color(80, 255, 255);
+     var hex = color.getHex();
+     var surface    = new Surface({
+         size: [300, 300],
+         properties: {
+             backgroundColor: hex
+         }
+     });
+     Context.add(surface);
 
+     var toggle = true;
+     surface.on('click', function(){
+         if (toggle) {
+             hex = color.setFromRGBA(255,0,0).getHex();
+         } else {
+             hex = color.setHue(60).getHex();
+         }
+         surface.setProperties({
+             backgroundColor: hex
+         })
+         toggle = !toggle;
+     });
 });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 define(function(require, exports, module) {
 	// import your example
-	require('examples/events/EventArbiter/example');
+	require('examples/utilities/Color/example');
 });
