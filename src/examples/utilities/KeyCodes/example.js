06632733e993a19daaab149f62acb4b8f4e6cc9b
Mike O'Brien <michael.obrien.a@gmail.com>
2014-03-22 08:46:51 -0700

    added keycodes example

--- src/examples/utilities/KeyCodes/example.js
+++ src/examples/utilities/KeyCodes/example.js
@@ -1,12 +1,23 @@
 define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
    
-    // create the main context
     var mainContext = Engine.createContext();
-    
-    
-    // your app here
-      
+    var surface = new Surface({
+    	content: 'start pressing some keys',
+    	properties: {
+	    	color: 'white'    		
+    	}
+    });
+    mainContext.add(surface);
 
+    Engine.on('keypress', function(event) {
+    	if (event.charCode >= 48 && event.charCode <= 57) {
+    		surface.setContent('you hit a number');
+    	} else {
+    		surface.setContent('not a number')
+    	}
+    });
 });
