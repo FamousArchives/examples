4323255fe1570687405f8d362ce4ebd464fbf358
Mike O'Brien <michael.obrien.a@gmail.com>
2014-03-22 08:00:08 -0700

    updated example json for single release version and added eventMapper example

--- src/examples/events/EventMapper/example.js
+++ src/examples/events/EventMapper/example.js
@@ -1,12 +1,25 @@
 define(function(require, exports, module) {
     // import dependencies
-    var Engine = require('famous/core/Engine');
+    var EventHandler = require('famous/core/EventHandler');
+    var EventMapper = require('famous/events/EventMapper');
    
-    // create the main context
-    var mainContext = Engine.createContext();
-    
-    
-    // your app here
+   var eventHandlerA = new EventHandler();
+   var eventHandlerB = new EventHandler();
+   var eventHandlerC = new EventHandler();
       
+	var myMapper = new EventMapper(function(type, data) {
+	    return (data && (data.direction === 'x')) ? eventHandlerB : eventHandlerC;
+	});
 
+	eventHandlerA.pipe(myMapper);
+
+	eventHandlerB.on('A', function(data){
+	    alert('B direction : ' + data.direction);
+	});
+	eventHandlerC.on('A', function(data){
+	    alert('C direction : ' + data.direction);
+	});
+
+	eventHandlerA.trigger('A', {direction : 'x'});  // pipes to eventHandlerB
+	eventHandlerA.trigger('A', {direction : 'y'});  // pipes to eventHandlerC
 });
