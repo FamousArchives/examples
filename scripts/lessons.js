13c4260824534c954e8567d05267151a43ba6397
Mike O'Brien <michael.obrien.a@gmail.com>
2014-05-14 14:56:23 -0700

    added push to aws

--- scripts/lessons.js
+++ scripts/lessons.js
@@ -3,6 +3,8 @@ var path = require('path');
 var esprima = require("esprima");
 var doctrine = require("doctrine");
 var async = require('async');
+var aws = require('aws-sdk');
+var s3 = new aws.S3();
 
 module.exports = function(jsonFiles, examplesPath, cb) {
     var lessons = {};
@@ -24,11 +26,42 @@ module.exports = function(jsonFiles, examplesPath, cb) {
 
                 lessons[version][lessonData.repo][lessonData.component].push(lessonData.example);
             }
+            fs.writeFileSync('build-' + version + '.json', JSON.stringify(lessons[version]));
+            uploadFile('examples/build-' + version + '.json', 'build-' + version + '.json')
         }
     }
-
-    fs.writeFile('build.json', JSON.stringify(lessons, null, 4), cb);
 }
+
+function uploadFile(remoteFilename, fileName) {
+  var fileBuffer = fs.readFileSync(fileName);
+  var metaData = getContentTypeByFile(fileName);
+  
+  s3.putObject({
+    ACL: 'public-read',
+    Bucket: 'famous-downloads',
+    Key: remoteFilename,
+    Body: fileBuffer,
+    ContentType: metaData
+  }, function(error, response) {
+    console.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
+    console.log(arguments);
+  });
+}
+
+function getContentTypeByFile(fileName) {
+  var rc = 'application/octet-stream';
+  var fn = fileName.toLowerCase();
+
+  if (fn.indexOf('.html') >= 0) rc = 'text/html';
+  else if (fn.indexOf('.css') >= 0) rc = 'text/css';
+  else if (fn.indexOf('.json') >= 0) rc = 'application/json';
+  else if (fn.indexOf('.js') >= 0) rc = 'application/x-javascript';
+  else if (fn.indexOf('.png') >= 0) rc = 'image/png';
+  else if (fn.indexOf('.jpg') >= 0) rc = 'image/jpg';
+
+  return rc;
+}
+
 
 function createIndividualLesson(version, name, example, examplesPath) {
     examplesPath = examplesPath.split('/');
